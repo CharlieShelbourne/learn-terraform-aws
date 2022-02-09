509409248add8cd1518ec5178960cf82df5d2294
Charlie Shelbourne <charlieshelbourne@charlies-mbp.home>
2022-02-09 21:10:22 +0000

    test

--- main.tf
+++ main.tf
@@ -26,14 +26,6 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-08d70e59c07c61a3a"
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = var.instance_name
-#   }
-# }
 
 resource "aws_s3_bucket" "b" {
   bucket = "bare-random-bucket-i-decide"
